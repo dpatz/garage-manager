1e8af4b90ee528c38cc226302f6db685fd3fddcc
Dan Patz <d@patz.me>
2020-02-13 21:40:07 -0800

    Add Purgecss

---
+++ docs/install_purgecss.md
@@ -0,0 +1,5 @@
+# Install Purgecss
+
+`npm install @fullhuman/postcss-purgecss --save-dev`
+
+Follow instructions [here](https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css)
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./**/*.tsx", "./**/*.ts"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("postcss-preset-env")]
+  plugins: [
+    require("tailwindcss"),
+    require("postcss-preset-env"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
 };
