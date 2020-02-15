8eb9aff49f6f8b27e9fe20abc5c0f1d6603a8d7c
Dan Patz <d@patz.me>
2020-02-15 13:48:04 -0800

    Move login page screenshot

--- cypress/integration/login_spec.ts
+++ cypress/integration/login_spec.ts
@@ -3,8 +3,11 @@ describe("The login form", function() {
     cy.visit("/login");
     cy.get('[name="email"]').type("dan@example.com");
     cy.get('[name="password"]').type("password");
-    cy.get("button").click();
-    cy.url().should("eq", "http://localhost:3000/");
+
     cy.percySnapshot();
+
+    cy.get("button").click();
+
+    cy.url().should("eq", "http://localhost:3000/");
   });
 });
