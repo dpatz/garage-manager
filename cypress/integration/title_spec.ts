4bbe9848b1c0ec786ef5ecc28db82da12375b893
Dan Patz <d@patz.me>
2020-05-17 20:51:57 -0700

    Remove unnecessary test

--- cypress/integration/title_spec.ts
+++
@@ -1,9 +0,0 @@
-describe("The app title", function () {
-  it("has the correct text content", function () {
-    cy.setCookie("token", "123");
-    cy.visit("/");
-    cy.get("header h1").should("contain", "GarageManager");
-    cy.percySnapshot();
-    cy.clearCookies();
-  });
-});
